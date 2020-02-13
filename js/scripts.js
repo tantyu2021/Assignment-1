6300e91e882a3a3070e5694a5ec7d9165f79dce2
tantyu2021 <tantyu2021@gmail.com>
2020-02-13 00:20:44 -0800

    add step1, step2, step3, step4 in scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+//STEP 1 -	Convert the identifiers to camelCase notation:
+var someMonth;
+function theMonth();
+var currentMonth;
+var summerMonth;
+var myLibraryFunction;
+
+//STEP 2 - an example of the literal expression
+5.4          // numeric literal
+"Wellcome!"  // string literal
+false        // Boolean literal
+true         // Boolean literal
+null         // null literal
+
+//STEP 3 - the examples of the complex/variable expression
+let someExpression = 6/5 + 7;
+let aThirdExpression = aSecondExpression + anExpression;
+
+//STEP 4 - declare 9 variables (without definition), use camelCase and Hungarian Notation 
+var fName;
+var address;
+var lName;
+var city;
+var state;
+var zipCode;
+var yourAge;
+var referralSource;
+var mayWeContactYou;
