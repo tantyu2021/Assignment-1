520e5bddfe69e80216c9845f6bceff06d6ceee08
tantyu2021 <tantyu2021@gmail.com>
2020-02-14 11:16:55 -0800

    add - step7 and corrections for 21 and 22 (Comparison and Logical and in other steps

--- js/scripts.js
+++ js/scripts.js
@@ -60,11 +60,18 @@ var total = 100;
 var frutty = "apple";
 window.console.log("We ate" + total + frutty);
 
-//STEP 7 -
+//STEP 7 -first variable - add Boolean and a string and display the coerced result; second variable - add a number and Boolean and display
+var tri1 = true;
+tri1 = "string after Boolean";
+alert(tri1);
+var tri2 = 7;
+tri2 = true;
+alert(tri2);
 
 //STEP 8 - string literal
 var someString = "Who once said, \"Only two things are infinite, the universe and human stupidity, and I'm not sure about the former. \"";
 window.console.log(someString);
+alert(someString);
 
 //STEP 9 -Create a variable that produces a null value in the console window. Create a variable that produces an undefined value in the console window.
 
@@ -98,16 +105,17 @@ typeof null      // returns object
 
 var firstName = "Zak"; 
 var lastName = "Ruvalcaba";
-alert("Hello" + firstName + "" + lastName + "Welcome to the JavaScript class!");
+alert("Hello," + " " + firstName + " " + lastName + "Welcome to the JavaScript class!");
 
 firstName = "Tatiana";
-LastName = "Bergman";
-alert("Hello" + firstName + "" + lastName + "Welcome to the JavaScript class!");
+lastName = "Bergman";
+alert("Hello" + firstName + " " + lastName + "Welcome to the JavaScript class!");
 
 
 //STEP 12 -Declare a variable called name and set it equal to your name. 
 var name = "Tatiana";
-alert("Hello" + name + ", welcome to the JavaScript class!");
+name = "Mona";
+alert("Hello," + " " + name + ", welcome to the JavaScript class!");
 
 //STEP 13 -
 var course = "JavaScript"; 
@@ -123,13 +131,14 @@ window.console.log(courseName);
 var name = prompt("What is your name?", name);
 
 //STEP 16 -
-var course = JavaScript;
+// var course = JavaScript; 
 var course = prompt("What class are you taking?", course);
 
 //STEP 17 - math, sum - 
 let x = 10;
 let y = 20;
-window.console.log(x + y);
+window.console.log(x + 20);
+// window.console.log(x + y);
 
 //STEP 18 - math, add and assign
 let x = 20;
@@ -146,16 +155,30 @@ let x = 20/3;
 x /= 1;
 window.console.log(x / 1);
 
-//STEP 21 - Logical operators, an application that evaluates to true and displays the result within the console window
-let x = 5;
-let y = 10;
-window.console.log(x == 5 && y == 10);
-
-//STEP 22 - Logical operators, an application that evaluates to false and displays the result within the console window
+//STEP 21 - Comparison and Logical operators, an application that evaluates to true and displays the result within the console window
 let x = 6;
 let y = 60;
-window.console.log(x === 6 && y ===60);
-
-let x = 10;
+window.console.log(x === 6 && y === 60);
+// or
+let x = 15;
+let y = 210;
+window.console.log(x == 15 || y == 210);
+// or
+let x = 5;
 let y = 10;
-window.console.log(x == y !=true);
+window.console.log(x < y == true);
+//and
+var city = prompt("Where do you live?");
+if (city == "San Diego") {
+    window.console.log("Fill the application.");
+}
+// or
+var city = prompt("Where do you live?");
+if (city == "San Diego") {
+    alert("Fill the application.");
+}
+
+//STEP 22 - Comparison and Logical operators, an application that evaluates to false and displays the result within the console window
+let x = 5;
+let y = 10;
+window.console.log(x = y != true);
